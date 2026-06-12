ee7cfdae207d0094de027e2e1417a6d5ce7d6d7d
a314 <a314@WS7-045.dn7-001.local>
2026-06-12 09:49:05 +0900

    年齢を表示するプログラムを作成しました

---
+++ src/main/java/com/example/demo/ProfileController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProfileController {
+	@GetMapping("/profile")
+	public String profile(
+			//defaultValueは文字列のみ受け取るb
+				@RequestParam(name = "age", required = false, defaultValue = "20")String age, Model model
+			) {
+		model.addAttribute("age", age);
+		
+		return "profile";
+	}
+
+}
